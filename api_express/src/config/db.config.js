30cc946b1f296f57f590c4eaf5b06a67a55a370e
Angel <angelherrarte3@gmail.com>
2024-03-05 20:44:32 -0600

    Add docker compose

--- api_express/src/config/db.config.js
+++ api_express/src/config/db.config.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize'
 
 const config = {
-  host: '127.0.0.1',
-  port: 33068,
+  host: 'mysql',
+  port: 3306,
   user: 'root',
   password: 'root_password',
   db: 'blog_db',
